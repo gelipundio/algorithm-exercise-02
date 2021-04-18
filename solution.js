aa8fe1e4e0d0948ff7dc2542630d13b7f493d31e
Klaus Hellemann <klaus@virgilio.io>
2021-04-18 10:03:30 -0500

    Solucion ahora si

---
+++ solution.js
@@ -0,0 +1,38 @@
+// function sumarize(inputArray) {
+//     let total = 0;
+//     if (inputArray) {
+//       for (let i = 0; i < inputArray.length; i++) {
+//         if (typeof inputArray[i] === "number") {
+//           total = total + inputArray[i];
+//         } else if (inputArray[i] instanceof Array) {
+//           total = total + sumarize(inputArray[i]);
+//         }
+//       }
+//       return total;
+//     }
+//     return 0;
+//   }
+
+//   module.exports = {
+//     sumarize,
+//   };
+
+function sumarize(inputArray) {
+  let total = 0;
+  if (inputArray) {
+    const newArray = inputArray.filter(Boolean);
+    for (let i = 0; i < newArray.length; i++) {
+      if (typeof newArray[i] === "number") {
+        total = total + newArray[i];
+      } else if (newArray[i] instanceof Array) {
+        total = total + sumarize(newArray[i]);
+      }
+    }
+    return total;
+  }
+  return 0;
+}
+
+module.exports = {
+  sumarize,
+};
