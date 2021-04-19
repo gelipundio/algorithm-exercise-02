8e4fb1c98e3a2495a100460d2922ece857752b16
davidLiath <davidliath@DESKTOP-9NO2UM7.localdomain>
2021-04-18 23:06:07 -0600

    Testing with Sol. of Ex. 1 (passed 10 out of 13)

--- solution.js
+++ solution.js
@@ -1,3 +1,14 @@
-module.export.sumarize =function () {
+module.exports.sumarize = function ( array ) {
+	        if ( array === null ) {
 
+			                return 0
+			        } else {
+
+					                var s = 0
+					                for ( var num of array ) {
+								                        s = s + num
+								                }
+
+					                return s
+					        }
 }
