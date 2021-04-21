321a20a15a2c4a67ee8fa2708060c95ffbd8d036
davidLiath <davidliath@DESKTOP-9NO2UM7.localdomain>
2021-04-21 14:39:15 -0600

    Trying to construct a recursive function

--- solution.js
+++ solution.js
@@ -1,14 +1,16 @@
 module.exports.sumarize = function ( array ) {
-	        if ( array === null ) {
-
-			                return 0
-			        } else {
-
-					                var s = 0
-					                for ( var num of array ) {
-								                        s = s + num
-								                }
-
-					                return s
-					        }
+	if (  array === "null" ) {
+		return 0
+	} else if {
+		var s = 0
+		for ( var num of array ) {
+			if ( typeof num == "array" ) {
+				num = this.sumarize( num )
+			} else if ( typeof num == "string" ) {
+				num = 0
+			}
+			s = s + num
+		}
+		return s
+	}
 }
