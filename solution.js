6e969e9a209ec7ac4cece229e750201119240046
davidLiath <davidliath@DESKTOP-9NO2UM7.localdomain>
2021-04-29 03:27:54 -0600

    Otra solución excluye booleanos, falta excluir {} y null al final

--- solution.js
+++ solution.js
@@ -1,16 +1,34 @@
-module.exports.sumarize = function ( array ) {
-	if (  array === "null" ) {
-		return 0
-	} else if {
-		var s = 0
-		for ( var num of array ) {
-			if ( typeof num == "array" ) {
-				num = this.sumarize( num )
-			} else if ( typeof num == "string" ) {
-				num = 0
-			}
-			s = s + num
-		}
-		return s
-	}
+function sumarize(array) {
+//CASO: Nulo 
+if ( array === null ) {
+return 0
+} else {
+let s = 0
+//CASO: Elemento es, a su vez, un Arreglo
+for (var num of array) {
+if (typeof num == "string") {
+num = parseInt(num)
+}
+if (typeof num == "boolean"){
+num = 0
+}
+let isArray = Array.isArray(num)
+if (isArray){
+num = sumarize(num)
+}
+if(typeof num != "number"){
+	num = 0
+}
+//const obj = {} 
+//if(num = obj){
+//	num = 0
+//}
+s = s + num
+}
+return s
+}
+}
+
+module.exports = {
+sumarize
 }
